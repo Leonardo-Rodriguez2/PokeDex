1dae15b11ce7a756eb155459362c9f5bdfb14aeb
Leonardo <leonardorodriguez172021@gmail.com>
2025-08-14 15:47:37 -0400

    cambio en los estilos y instalando tailwind

--- src/app/components/pokemon.card/pokemon.card.component.ts
+++ src/app/components/pokemon.card/pokemon.card.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-pokemon.card',
-  imports: [],
+  selector: 'app-pokemon-card',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pokemon.card.component.html',
-  styleUrl: './pokemon.card.component.css'
+  styleUrls: ['./pokemon.card.component.css']
 })
 export class PokemonCardComponent {
+  @Input() pokemons: any[] = [];
+  // Usa @Output para emitir un evento
+  @Output() cardClicked = new EventEmitter<any>();
 
+  onCardClick(pokemon: any): void {
+    // Cuando se hace clic, emite el evento con los datos del Pokémon
+    this.cardClicked.emit(pokemon);
+  }
 }
--- src/app/pages/pokemons.page/pokemons.page.component.ts
+++ src/app/pages/pokemons.page/pokemons.page.component.ts
@@ -5,15 +5,19 @@ import { switchMap, catchError } from 'rxjs/operators';
 import { of, forkJoin } from 'rxjs';
 import { PokemonDetailsModalComponent } from '../../components/pokemon.details.modal/pokemon.details.modal.component';
 import { SearchFilterBarComponent } from '../../components/search.filter.bar/search.filter.bar.component';
+import { PokemonCardComponent } from '../../components/pokemon.card/pokemon.card.component';
 
 @Component({
   selector: 'app-pokemons.page',
   standalone: true,
-  imports: [CommonModule, PokemonDetailsModalComponent,
-    SearchFilterBarComponent
+  imports: [
+    CommonModule,
+    PokemonDetailsModalComponent,
+    SearchFilterBarComponent,
+    PokemonCardComponent,
   ],
   templateUrl: './pokemons.page.component.html',
-  styleUrl: './pokemons.page.component.css'
+  styleUrl: './pokemons.page.component.css',
 })
 export class PokemonsPageComponent {
   public pokemons: any[] = [];
@@ -37,72 +41,86 @@ export class PokemonsPageComponent {
     this.isLoading = true;
     this.hasError = false;
 
-    this.pokemonsListService.listPokemons(this.limit).pipe(
-      switchMap(response => {
-        const detailRequests = response.results.map((pokemon: any) =>
-          this.pokemonsListService.getPokemonDetails(pokemon.url)
-        );
-        return forkJoin(detailRequests);
-      }),
-      catchError(error => {
-        console.error('Error al obtener los datos de Pokémon:', error);
-        this.hasError = true;
-        this.isLoading = false;
-        return of([]);
-      })
-    ).subscribe({
-      next: (data: any) => {
-        if (Array.isArray(data)) {
-          this.pokemons = data.map((pokemon: any) => ({
-            id: pokemon.id,
-            name: pokemon.name,
-            type: pokemon.types[0]?.type.name || 'Unknown',
-            image: pokemon.sprites?.other?.['official-artwork']?.front_default || pokemon.sprites?.front_default || '',
-            speciesUrl: pokemon.species.url,
-            height: pokemon.height,
-            weight: pokemon.weight,
-          }));
-          console.log(data);
-        } else {
-          console.error('Los datos recibidos no son un array:', data);
+    this.pokemonsListService
+      .listPokemons(this.limit)
+      .pipe(
+        switchMap((response) => {
+          const detailRequests = response.results.map((pokemon: any) =>
+            this.pokemonsListService.getPokemonDetails(pokemon.url)
+          );
+          return forkJoin(detailRequests);
+        }),
+        catchError((error) => {
+          console.error('Error al obtener los datos de Pokémon:', error);
           this.hasError = true;
-        }
-        this.isLoading = false;
-      },
-      error: (error) => {
-        console.error('Error al cargar los Pokémon:', error);
-        this.hasError = true;
-        this.isLoading = false;
-      }
-    });
+          this.isLoading = false;
+          return of([]);
+        })
+      )
+      .subscribe({
+        next: (data: any) => {
+          if (Array.isArray(data)) {
+            this.pokemons = data.map((pokemon: any) => ({
+              id: pokemon.id,
+              name: pokemon.name,
+              type: pokemon.types[0]?.type.name || 'Unknown',
+              image:
+                pokemon.sprites?.other?.['official-artwork']?.front_default ||
+                pokemon.sprites?.front_default ||
+                '',
+              speciesUrl: pokemon.species.url,
+              height: pokemon.height,
+              weight: pokemon.weight,
+            }));
+            console.log(data);
+          } else {
+            console.error('Los datos recibidos no son un array:', data);
+            this.hasError = true;
+          }
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.error('Error al cargar los Pokémon:', error);
+          this.hasError = true;
+          this.isLoading = false;
+        },
+      });
   }
 
   openModal(pokemon: any): void {
     this.selectedPokemon = pokemon;
     this.isLoading = true;
     // Eliminamos esta línea para que el modal no se abra antes de tener los datos.
-    // this.isModalOpen = true; 
+    // this.isModalOpen = true;
 
     this.pokemonsListService.getPokemonSpecies(pokemon.speciesUrl).subscribe({
       next: (speciesData: any) => {
         this.selectedPokemonDetails = {
-          category: speciesData.genera.find((g: any) => g.language.name === 'en')?.genus || 'Unknown',
+          category:
+            speciesData.genera.find((g: any) => g.language.name === 'en')
+              ?.genus || 'Unknown',
           gender: 'Male - Female',
           habitat: speciesData.habitat?.name || 'Unknown',
           color: speciesData.color?.name || 'Unknown',
-          description: speciesData.flavor_text_entries.find((entry: any) => entry.language.name === 'en')?.flavor_text || 'No description available.',
-          evolutionChainUrl: speciesData.evolution_chain.url
+          description:
+            speciesData.flavor_text_entries.find(
+              (entry: any) => entry.language.name === 'en'
+            )?.flavor_text || 'No description available.',
+          evolutionChainUrl: speciesData.evolution_chain.url,
         };
         // Movemos la apertura del modal aquí para asegurar que los datos estén listos.
-        this.isModalOpen = true; 
+        this.isModalOpen = true;
         this.isLoading = false;
       },
       error: (error) => {
         console.error('Error al obtener los detalles de la especie:', error);
         this.hasError = true;
         this.isLoading = false;
-      }
+      },
     });
+
+
+    console.log('modal activa')
   }
 
   closeModal(): void {
